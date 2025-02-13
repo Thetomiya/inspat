41eff94e10eafc8ceb8d83cd57fc1c411092f7f3
Ilya <ilyacisenko@gmail.com>
2025-02-13 16:28:36 +0300

    added 3 new sections at index

--- index.php
+++ index.php
@@ -18,7 +18,6 @@
         <div class="container-fluid  p-0">
             <a class="navbar-brand" href="index.php"><img src="img/logo.svg" alt="logo.svg"></a>
 
-            <!-- Контейнер buttons-wrap с изменяемым порядком -->
             <div class='d-flex buttons-wrap order-lg-2'>
                 <div class="navbar-switches p-0 d-flex justify-content-between">
                     <div class="theme-switch-container ps-3 pe-3 align-content-center">
@@ -33,7 +32,6 @@
                 </button>
             </div>
 
-            <!-- Блок collapse с изменяемым порядком -->
             <div class="collapse row drop-menu navbar-collapse justify-content-center justify-content-lg-between order-lg-1" id="navbarNavAltMarkup">
                 <div class="row flex-column  flex-lg-row p-0">
                     <div class="navbar-nav  p-0 col d-flex text-center justify-content-center">
@@ -49,19 +47,105 @@
     </div>
 
     <main>
-    <section class="hero">
-        <!-- Герой-секция с картинкой -->
-        <div class="hero-content">
-            <h1>Добро пожаловать!</h1>
-            <p>Это пример герой-секции</p>
-        </div>
-    </section>
+        
+        <section class="hero">
+            <div class="container ">
+                <div class="hero-content col-12 col-lg-7 col-xl-5">
+                    <h1 class="h1">Создаем пространства будущего</h1>
+                    <p>Мы создаём вдохновляющие пространства, работая с жилыми, коммерческими и промышленными объектами. Наша команда воплощает ваши идеи, обеспечивая высокое качество и креативность в каждом проекте.</p>
 
-    <section class="content">
-        <!-- Остальной контент -->
-        <h2>Дополнительный контент</h2>
-        <p>Этот контент появится после прокрутки.</p>
-    </section>
+                    <div class="btns-wrap">
+                        <a href="#" class="hero-btn-primary">Связаться</a>
+                        <a href="#" class="hero-btn-secondary">Наши проекты</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="page-block">
+            <div class="container d-flex flex-column flex-lg-row align-items-center justify-content-between">
+                <div class="me-0 me-lg-3">
+                    <h2 class="h2">Опыт и знания в различных типах архитектурных проектов</h2>
+                    <p class="p-title">Наше бюро имеет обширный опыт проектирования жилых, коммерческих и сложных промышленных объектов. Мы применяем инновационные подходы и стремимся к выдающимся результатам, учитывая уникальные запросы каждого клиента.</p>
+                </div>
+                <img src="img/temp-img.png" class="page-block-img col-12 col-lg-6">
+            </div>
+        </section>
+
+        <section class="page-block">
+            <div class="container">
+                <h2 class="h2 col-12 col-lg-6 mb-5">Инновационные решения для общественных и жилых зданий</h2>
+
+                <div class="row">
+                    <div class="col-md-4 mb-md-0 mb-5 col-12">
+                        <a href="#" class="project-card-preview">
+                            <img src="img/temp-img.png" class="img-fluid mb-4" alt="image-preview">
+                            <h5 class="h5 mb-4">ЖК «ОКТЯБРЬСКАЯ»</h5>
+                            <p class="project-card-description">Современный жилой комплекс с продуманными планировочными решениями и удобной инфраструктурой для комфортной жизни в центре столицы.</p>
+                            <span class="project-card-description">Подробнее ></span>
+                        </a>
+                    </div>
+                    <div class="col-md-4 mb-md-0 mb-5 col-12">
+                        <a href="#" class="project-card-preview">
+                            <img src="img/temp-img.png" class="img-fluid mb-4" alt="image-preview">
+                            <h5 class="h5 mb-4">ГОСТИНИЧНЫЙ КОПЛЕКС</h5>
+                            <p class="project-card-description">Комфортная 4-звёздочная гостиница с рестораном, конференц-залами и спортивными помещениями для полноценного отдыха гостей.</p>
+                            <span class="project-card-description">Подробнее ></span>
+                        </a>
+                    </div>
+                    <div class="col-md-4 mb-md-0 mb-5 col-12">
+                        <a href="#" class="project-card-preview">
+                            <img src="img/temp-img.png" class="img-fluid mb-4" alt="image-preview">
+                            <h5 class="h5 mb-4">РЕКОНСТРУКЦИЯ ЗДАНИЯ</h5>
+                            <p class="project-card-description">Восстановление исторических фасадов с современными архитектурными решениями и созданием комфортного рабочего пространства.</p>
+                            <span class="project-card-description">Подробнее ></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="page-block">
+            <div class="container">
+                <div class="row mb-5 justify-content-between">
+                    <h2 class="h2 col-12 col-md-5 mb-5">Преобразуем пространства для жизни и города</h2>
+                    <p class="p-title col-12 col-md-6">Мы создаём современные жилые здания, общественные пространства и продуманные градостроительные решения. Наши проекты гармонично сочетают функциональность, эстетику и передовые технологии, формируя комфортную среду для жизни, работы и отдыха. Мы учитываем уникальные особенности каждого пространства и внедряем устойчивые решения для улучшения городской среды.</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6 mb-md-0 mb-4 col-12">
+                        <div class="project-card-preview">
+                            <img src="img/temp-1.png" class="img-fluid mb-4" alt="image-preview">
+                            <h6 class="h6 mb-4">Общественные и административные здания</h5>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 mb-md-0 mb-4 col-12">
+                        <div class="project-card-preview">
+                            <img src="img/temp-2.png" class="img-fluid mb-4" alt="image-preview">
+                            <h6 class="h6 mb-4">Жилые здания</h5>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 mb-md-0 mb-4 col-12">
+                        <div class="project-card-preview">
+                            <img src="img/temp-3.png" class="img-fluid mb-4" alt="image-preview">
+                            <h6 class="h6 mb-4">Медицинские и фармацевтические объекты</h5>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 mb-md-0 mb-4 col-12">
+                        <div class="project-card-preview">
+                            <img src="img/temp-4.png" class="img-fluid mb-4" alt="image-preview">
+                            <h6 class="h6 mb-4">Промышленные объекты</h5>
+                        
+                        </div>
+                    </div>
+                    
+                </div>
+
+            </div>
+        </section>
+        
     </main>
 
 
